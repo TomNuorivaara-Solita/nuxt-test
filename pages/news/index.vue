3c461772452ec70b206543f4f51f6815b5e0de96
TomNuorivaara-Solita <tom.nuorivaara@solita.fi>
2017-06-29 15:41:45 +0300

    filter added

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -6,6 +6,7 @@
             <div class="row" style="margin-top:4rem;">
                 <div class="col-xs">
                     <nuxt-link to="/"><button class="button"><i class="button__icon material-icons md-light md-md">home</i><span class="button__text">Etusivu</span></button></nuxt-link>
+                    <button class="button" @click="toggleFilter"><i class="button__icon material-icons md-light md-md">home</i><span class="button__text">Filter</span></button>
                 </div>
             </div>
             <div class="row">
@@ -13,7 +14,7 @@
                     <h1 class="h1">Tällä sivulla on uutisia.</h1>
                 </div>
             </div>
-            <section v-for="article in news" :key="article">
+            <section v-for="article in items" :key="article">
                 <div class="row">
                     <div class="col-xs">
                         <h2 class="h2">{{ article.title }}</h2>
@@ -34,8 +35,23 @@ export default {
   components: {
     TopNav
   },
+  methods: {
+    toggleFilter () {
+      this.isFiltered = !this.isFiltered
+    }
+  },
+  computed: {
+    items () {
+      console.log(this.news)
+      if (this.news.length === 0) {
+        return this.news
+      }
+      return this.isFiltered ? this.news.slice(0, 1) : this.news
+    }
+  },
   data () {
     return {
+      isFiltered: false,
       news: []
     }
   },
